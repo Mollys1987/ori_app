0e867d95df96fdaebe4397ab6423bd0e1600eb54
EC2 Default User <ec2-user@ip-172-31-7-12.ap-northeast-1.compute.internal>
2021-10-29 03:05:43 +0000

    comment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,20 @@
 class SearchController < ApplicationController
   def search
-    if params[:nickname] or params[:area] or params[:title].present?
-      @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%")
-      @areas = User.where('city LIKE ?', "%#{params[:city]}%")
-      @posts = Post.where('title LIKE ?', "%#{params[:title]}%")
-    else
-      @users = User.none
-      @areas = User.none
-      @posts = Post.none
-      # flash[:notice] = "一致する結果がありません"
-    end
+    p "==========="
+    p params
+    p "==========="
+    @multis = User.where('key_word1 LIKE ? or key_word2 LIKE ? or key_word3 LIKE ?', "%#{params[:multi]}%", "%#{params[:multi]}%", "%#{params[:multi]}%")
+    # if params[:nickname] or params[:area] or params[:title].present?
+    #   @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%")
+    #   @areas = User.where('city LIKE ?', "%#{params[:city]}%")
+    #   @posts = Post.where('title LIKE ?', "%#{params[:title]}%")
+    # else
+    #   @multis = User.none
+    #   @users = User.none
+    #   @areas = User.none
+    #   @posts = Post.none
+    #   # flash[:notice] = "一致する結果がありません"
+    # end
   end
   
   def s_ajax
@@ -17,9 +22,32 @@ class SearchController < ApplicationController
   end
   
   def result
-    @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%")
-    respond_to do |format|
-      format.json { render json: {users: @users} }
+    p "==========="
+    p params
+    p "==========="
+    if params[:nickname]
+      @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%")
+      respond_to do |format|
+        format.json { render json: {users: @users} }
+      end
+    end
+    if params[:city]
+      @cities = User.where('city LIKE ?', "%#{params[:city]}%")
+      respond_to do |format|
+        format.json { render json: {cities: @cities} }
+      end
+    end
+    if params[:key]
+     @keies = User.where('key_word1 LIKE ? or key_word2 LIKE ? or key_word3 LIKE ?', "%#{params[:key]}%", "%#{params[:key]}%", "%#{params[:key]}%")
+      respond_to do |format|
+        format.json { render json: {keies: @keies} }
+      end
+    end
+    if params[:title]
+      @titles = Post.where('title LIKE ?', "%#{params[:title]}%").as_json(include: :user)
+      respond_to do |format|
+        format.json { render json: {titles: @titles} }
+      end
     end
   end
   
